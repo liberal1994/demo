22fae0275945fb233b7e0edb99c737e5564d34a7
liberal <liberal1994@163.com>
2018-10-09 00:05:25 +0800

    demo

---
+++ test/src/sort/InsertSort.java
@@ -0,0 +1,14 @@
+package sort;
+
+import java.util.Arrays;
+
+public class InsertSort {
+    public static void main(String[] args) {
+        int[] arr = {23, 26, 5, 19, 52, 17, 28, 63, 77, 29};
+        System.out.println(Arrays.toString(arr));
+
+    }
+    public static void insertSort(){
+
+    }
+}
