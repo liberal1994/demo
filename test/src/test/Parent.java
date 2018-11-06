f6e886e09c0bd2780864f1808862d1765823724b
liberal <liberal1994@163.com>
2018-11-06 18:44:21 +0800

    sa

---
+++ test/src/test/Parent.java
@@ -0,0 +1,22 @@
+package test;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class Parent {
+    public static final void test1() {
+    }
+
+    public static void test2() {
+    }
+
+    public final void test3() {
+    }
+
+    private final void test4() {
+    }
+    private static void test5(){}
+
+    protected Parent test6() throws Exception {return null;}
+
+}
---
+++ test/src/test/Son.java
@@ -0,0 +1,13 @@
+package test;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class Son extends Parent{
+    public void test4(){}
+//    public void test3(){}
+//    public void test2(){}
+//    public void test1(){}
+    public void test5(){}
+    protected Son test6() throws IOException,ArithmeticException,Exception {return null;}
+}
